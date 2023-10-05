825801617bd84fbc00bbb71e434320c5a62e9f41
Satyam Raj <rajsatyammm@gmail.com>
2023-10-05 14:37:49 +0530

    Initial Commit: Created an React app using Vite and configured TailwindCSS into it

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+	return (
+		<div className="w-screen min-h-screen font-bold text-red-60 overflow-x-hidden relative">
+			<p className="font-bold text-4xl">hello</p> 
+		</div>
+	)
+}
+
+export default App
